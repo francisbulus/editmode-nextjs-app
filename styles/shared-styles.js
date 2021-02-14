bd5c2af2bfd43da30079dbdc6b7324670c47cc79
Francis Bulus <notchera@gmail.com>
2021-02-14 23:01:22 +0100

    create global style export as a style wrapper for page

--- styles/shared-styles.js
+++ styles/shared-styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import media from "./media";
+import media from "./media-queries";
 import { css } from "styled-components";
 import theme from "./theme";
 const { colors, fontSizes, fonts } = theme;
