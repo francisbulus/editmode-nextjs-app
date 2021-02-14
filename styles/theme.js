3b785a2d18fda5db76f320ec6e6560c70c4fd70b
Francis Bulus <notchera@gmail.com>
2021-02-14 22:45:43 +0100

    create theme with colors, fonts, and element sizes for reuse

---
+++ styles/theme.js
@@ -0,0 +1,40 @@
+export default {
+  preload: [
+    "https://fonts.googleapis.com/css?family=Inconsolata|La+Belle+Aurore&display=swap",
+    "https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap",
+  ],
+  colors: {
+    darkNavyStart: "#1C3451",
+    darkNavyEnd: "#0C1F2C",
+    lightYellow: "#FCE257",
+    darkYellow: "#FCDD3B",
+    bookYellow: "#E5E5E5",
+    darkGrey: "#828D99",
+    textNavy: "#1D344E",
+    titleWhite: "#E5E5E5",
+    hoverYellow: "#EAD87B",
+    cardGrey: "#2A405C",
+    lightGrey: "#ADBBCA",
+    darkNavyEndLite: "#163245",
+  },
+  borderRadius: "3px",
+  fonts: {
+    FuturaPT:
+      "Futura PT, Montserrat, Roboto, -apple-system, system-ui, BlinkMacSystemFont",
+    FuturaPTLight:
+      "Futura PT Light, Montserrat, Roboto, -apple-system, system-ui, BlinkMacSystemFont",
+    Inconsolata: "Roboto Mono, Inconsolata, Monaco, monospace",
+    LaBelle: "La Belle Aurore, cursive",
+  },
+
+  fontSizes: {
+    xxs: "12px",
+    xs: "14px",
+    sm: "15.5px",
+    md: "18px",
+    l: "20px",
+    xl: "22px",
+    xxl: "25px",
+    ttl: "34px",
+  },
+};
